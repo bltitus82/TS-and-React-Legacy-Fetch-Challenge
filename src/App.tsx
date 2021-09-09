2d62fec92dd248d9869233181b047d0500d186b5
Brad Titus <bltitus@me.com>
2021-09-09 19:54:02 -0400

    added functionality to get location and pull API data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'init',
+      isLoaded: false,
+      weatherData: null
+    }
+  }
+
+  abortController = new AbortController();
+  controllerSignal = this.abortController.signal;
+
+  weatherInit = () => {
+    const success = (position) => {
+      this.getWeatherData(position.coords.latitude, position.coords.longitude);
+    }
+
+    const error = () => {
+      alert('Unable to retrieve location.');
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      alert('Your browser does not support location tracking, or permission is denied.');
+    }
+  }
+
+  getWeatherData = () = {
+    const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+
+    fetch(weatherApi, { signal: this.controllerSignal })
+    .then(response => response.json())
+    .then(
+      (result) => {
+
+      console.log(result);
+
+      const { name } = result;
+      const { country } = result.sys;
+      const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
+      const { description, icon } = result.weather[0];
+      const { speed, deg } = result.wind;
+
+      this.setState({
+        isLoaded: true,
+        weatherData: {
+          name,
+          country,
+          description,
+          icon,
+          temp: temp.toFixed(1),
+          feels_like: feels_like.toFixed(1),
+          temp_min: temp_min.toFixed(1),
+          temp_max: temp_max.toFixed(1),
+          speed,
+          deg,
+          humidity
+        }
+      });
+    },
+    (error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
+  );
+  }
+
+  componentDidMount() {
+    this.weatherInit();
+  }
+
+  componentWillUnmount() {
+    this.abortController.abort();
+  }
+
+render() { 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
